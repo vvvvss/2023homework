dabef473ffac2638f01597264c80e428fa286130
vvvvss <songweiyi0318@gmail.com>
2023-09-12 09:46:58 +0800

    MongoDB Atlas

--- homework5/demo/demo/src/main/java/com/homework5/demo/SightRepository.java
+++ homework5/demo/demo/src/main/java/com/homework5/demo/SightRepository.java
@@ -1,9 +1,10 @@
 package com.homework5.demo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface SightRepository extends MongoRepository<Sight, String> {
     List<Sight> findByZoneLike(String sightZone);
 }
